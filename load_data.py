71053c53cfd4a9081c93330ead37224291b38dd5
JuleTrotzki <tj4062@mci4me.at>
2024-04-11 16:08:35 +0200

    first tries

--- load_data.py
+++ load_data.py
@@ -28,4 +28,10 @@ if __name__ == "__main__":
     print(power_W)
     sorted_power_W = bubble_sort(power_W)
     print(sorted_power_W[::-1])
+    
+    
+    
+    
+plt.plot(sorted_power_W)
+plt.show()
   
---
+++ main.py
@@ -0,0 +1,8 @@
+# nur Daten von Power sortieren über die Zeit
+# von groß nach klein
+
+import matplotlib.pyplot as plt
+
+
+plt.plot(sorted_power_W)
+plt.show()
