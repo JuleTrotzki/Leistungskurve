ca94dff5b2d4151f6bd87d89a1ea03111f4ab112
JuleTrotzki <tj4062@mci4me.at>
2024-04-12 18:48:51 +0200

    korrigierte sort file

--- main.py
+++ main.py
@@ -1,8 +1,1 @@
-# nur Daten von Power sortieren über die Zeit
-# von groß nach klein
 
-import matplotlib.pyplot as plt
-
-
-plt.plot(sorted_power_W)
-plt.show()
--- sort.py
+++ sort.py
@@ -4,7 +4,16 @@ def bubble_sort(arr):
         
         for j in range(0, n-i-1):
          
-            if arr[j] > arr[j+1]:
-                arr[j], arr[j+1] = arr[j], arr[j+1]
+            if arr[j] < arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
 
     return arr
+
+
+#Test: 
+if __name__ == "__main__":
+    test_array = [4, 5 ,1, 8, 2, 12, 457, 5, 7, 6797]
+    print(test_array)
+    sorted_arry = bubble_sort(test_array)
+    
+    print(sorted_arry)
